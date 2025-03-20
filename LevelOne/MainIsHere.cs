f16e1df6e258886cf5fb150e98909ac3e5434a1d
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-20 23:46:18 +0000

    Add method doc

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -3,7 +3,11 @@
 namespace LevelOne
 {
     public class Program
-    {
+    {  
+        /// <summary>
+        /// the main method
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
