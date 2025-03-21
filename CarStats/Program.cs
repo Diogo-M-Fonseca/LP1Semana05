4b51921c87e2eac74ebcc63c949386eecf39d7c4
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-21 17:47:34 +0000

    first try

---
+++ CarStats/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Bogus;
+using Spectre.Console;
+
+namespace CarStats
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            BarChart bc = new BarChart();
+            bc.AddItem("Dacia",12);
+
+            Randomizer.Seed = new Random(int.Parse(args[0]));
+            Faker faker = new Faker();
+
+        }
+    }
+}
