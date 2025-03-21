cf7e0f4f56af152956ff917ec50b084ac0c20359
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-21 00:16:01 +0000

    Add color print

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -14,7 +14,8 @@ namespace LevelOne
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
-            Console.WriteLine("Bye!")
+            Console.WriteLine("Nice day!");
+            Console.WriteLine("Bye!");
         }
         /// <summary>
         /// the printHelloworld method
